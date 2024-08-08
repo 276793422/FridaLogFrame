f7c616b977f395222da45f854f25f748fd69425e
zhaojz5 <zhaojz5@lenovo.com>
2024-08-08 21:44:19 +0800

    FIX Reformat

--- inject.js
+++ inject.js
@@ -16,198 +16,73 @@
 //                      第二个参数是隐含的默认参数 this ，内部包含了寄存器信息等
 //      Post 函数用得到的也就一个参数 返回值
 //
+//  key 字段用来存放当前节点的可预置索引，用来查找对应的global
+//  global 字段用来存放当前节点的全局变量，用来保存所需的全局数据，主要是用来存放从 Pre 到 Post 流程之间的内容
+//
 //  唯一不确定的情况，是开启优化编译的情况下，当前模块的相关问题，参数是否能正确解析
 //  https://frida.re/docs/javascript-api/
 var module_function_windows = [
-/******************************************************************************
-//  CreateFileW 示例
-{'global': {}, 'module': 'kernelbase.dll', 'func': 'CreateFileW',
-    //  https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi
-    //  https://docs.microsoft.com/en-us/windows/win32/fileio/creating-and-opening-files
-    //  后三个参数可能不正确，因为这里没有用标准方式取后三个参数
-    'pre': function (args) {
-        console.log('[+] Called CreateFileW');
-        console.log('\t arg 1 addr : ' + this.context.rcx);
-        console.log('\t arg 1 : ' + UnicodeByteToString(this.context.rcx));
-        console.log('\t arg 2 : ' + this.context.rdx);
-        console.log('\t arg 3 : ' + this.context.r8);
-        console.log('\t arg 4 : ' + this.context.r9);
-        console.log('\t arg 5 : ' + this.context.r12);
-        console.log('\t arg 6 : ' + this.context.rdi);
-        console.log('\t arg 7 : ' + this.context.r13);
-        //  保存输出参数地址
-        module_function_windows[1].global[this.context.ebp] = this.context.rcx
-    },
-    'post': function (return_value) {
-        //  函数返回的时候，覆盖这个输出参数地址
-        console.log('\r ---->');
-        console.log('\t arg 1 : ' + module_function_windows[1].global[this.context.ebp]);
-        console.log('[+] Returned from CreateFileW : ' + return_value);
-        console.log('');
-        delete module_function_windows[1].global[this.context.ebp];
-    } },
-******************************************************************************/
-
-/******************************************************************************
-{'global': {}, 'module': 'user32.dll', 'func': 'SetWindowTextA', 'pre': function (args) {
-        //console.log('[+] Called SetWindowTextA');
-        //for (var i = 0; i < 2; i++)
-        //{
-        //    console.log('param    : '+i+': ' + args[i]);
-        //}
-        //console.log('Text     : ' + this.context.esp.add(0x8).readPointer().readCString())
-        //console.log('Text     : ' + args[1].readCString())
-        //console.log('this     : ' + JSON.stringify(this));
-        //console.log('Context  : ' + JSON.stringify(this.context));
-        //NotifyRemotePythonInformation('SetWindowTextA', args[1], args[1].readCString().length)
-    }, 'post': function (return_value) {
-        //console.log('[+] Returned from SetWindowTextA : ' + return_value);
-        //console.log('');
-    } },
-******************************************************************************/
-
 ///******************************************************************************
 //  GetWindowTextA 劫持返回值示例
-{'global': {}, 'module': 'user32.dll', 'func': 'GetWindowTextA',
+{'key' : 'user32_GetWindowTextA' , 'global': {}, 'module': 'user32.dll', 'func': 'GetWindowTextA',
     'pre': function (args) {
         console.log('[+] Called GetWindowTextA');
-        console.log('Context  : ' + JSON.stringify(this.context));
+        var params = this.context;
+        var obj = GetContext(module_function_windows, 'user32_GetWindowTextA');
+
         //  保存输出参数地址
-        module_function_windows[1].global[this.context.ebp] = args[1];
-        console.log('args[1]  : ' + module_function_windows[1].global[this.context.ebp]);
+        console.log('Context  : ' + JSON.stringify(params));
+        console.log('args[1]  : ' + args[1]);
+        console.log('args[2]  : ' + args[2]);
+        obj.global[params.ebp] = args[1];
     },
     'post': function (return_value) {
         //  函数返回的时候，覆盖这个输出参数地址
-        console.log('args[1]  : ' + module_function_windows[1].global[this.context.ebp]);
-        module_function_windows[1].global[this.context.ebp].writeAnsiString("123456789");
-        console.log('this     : ' + JSON.stringify(this));
-        console.log('Context  : ' + JSON.stringify(this.context));
+        //console.log("\nBacktrace:\n" + Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join("\n"));
+        var params = this.context;
+        var obj = GetContext(module_function_windows, 'user32_GetWindowTextA');
+
+        console.log('args[1]  : ' + obj.global[params.ebp]);
+        console.log('Context  : ' + JSON.stringify(params));
         console.log('[+] Returned from GetWindowTextA : ' + return_value);
+
+        obj.global[params.ebp].writeAnsiString("123456789");
         console.log('');
-        delete module_function_windows[1].global[this.context.ebp];
+        delete obj.global[params.ebp];
     } },
+{'key' : 'kernelbase_readfile' , 'global': {}, 'module': 'kernelbase.dll', 'func': 'ReadFile',
+    'pre': function (args) {
+        console.log('[+] Called KernelBase!ReadFile');
+        var params = this.context;
+        var obj = GetContext(module_function_windows, 'kernelbase_readfile');
+
+        obj.global[params.rbp] = [params.rcx, params.rdx, params.r8, params.r9];
+        console.log('args  : ' + obj.global[params.rbp]);
+    },
+    'post': function (return_value) {
+        var params = this.context;
+        var obj = GetContext(module_function_windows, 'kernelbase_readfile');
+
+        var [hFile, lpBuffer, nNumberOfBytesToRead, pReadCount] = obj.global[params.rbp];
+        delete obj.global[params.rbp];
+        console.log('hFile                 : ' + hFile);
+        console.log('lpBuffer              : ' + lpBuffer);
+        console.log('nNumberOfBytesToRead  : ' + nNumberOfBytesToRead);
+        console.log('pReadCount            : ' + pReadCount);
+        console.log('[+] Returned from KernelBase!ReadFile : ' + return_value);
+
+        var dmp = OutputBuffer(lpBuffer, 0, nNumberOfBytesToRead);
+        console.log('hexdump : ');
+        console.log(dmp);
+        console.log('');
+    }
+},
 //******************************************************************************/
 
-/*
-{'global': {}, 'module': 'GameAssembly.dll', 'offset': 0x014BDB90,
-	'pre': function (args) {
-        console.log('[+] call pre  UnityEngine.Input::GetKeyInt(UnityEngine.KeyCode)');
-        module_function_windows[1].global[this.context.ebp] = args[1];
-    },
-	'post': function (return_value) {
-		console.log('[+] call post UnityEngine.Input::GetKeyInt(UnityEngine.KeyCode)');
-        console.log('args[1]  : ' + module_function_windows[1].global[this.context.ebp]);
-        console.log('return_value : ' + return_value);
-	} },
-*/
-
-/*
-{'global': {}, 'module': 'GameAssembly.dll', 'offset': 0x014BDB50,
-	'pre': function (args) {
-        //console.log('[+] call pre  UnityEngine.Input::GetKeyDownInt(UnityEngine.KeyCode)');
-        module_function_windows[1].global[this.context.ebp] = this.context.rcx;
-    },
-	'post': function (return_value) {
-		if (return_value == 0) {
-			return;
-		}
-		console.log('  -->');
-		console.log(GetCurrentTime() + '[+] call post UnityEngine.Input::GetKeyDownInt(UnityEngine.KeyCode)');
-		var key = module_function_windows[1].global[this.context.ebp];
-        console.log('args[1]  : ' + key + ' >>>> return_value : ' + return_value);
-        if (key == 0x64) return_value.replace(0x1);
-        if (key == 0x77) return_value.replace(0x1);
-	} },
-{'global': {}, 'module': 'GameAssembly.dll', 'offset': 0x014BDBD0,
-	'pre': function (args) {
-        //console.log('[+] call pre  UnityEngine.Input::GetKeyUpInt(UnityEngine.KeyCode)');
-        module_function_windows[1].global[this.context.ebp] = this.context.rcx;
-    },
-	'post': function (return_value) {
-		if (return_value == 0) {
-			return;
-		}
-		console.log('  ==>');
-		console.log(GetCurrentTime() + '[+] call post UnityEngine.Input::GetKeyUpInt(UnityEngine.KeyCode)');
-		var key = module_function_windows[1].global[this.context.ebp];
-        console.log('args[1]  : ' + key + ' >>>> return_value : ' + return_value);
-        if (key == 0x77) return_value.replace(0x1);
-	} },
-*/
-
-/*
-{'global': {}, 'module': 'GameAssembly.dll', 'offset': 0x014BDCC0,
-	'pre': function (args) {
-        //console.log('[+] call pre  UnityEngine.Input::GetMouseButtonDown(UnityEngine.KeyCode)');
-        module_function_windows[1].global[this.context.ebp] = this.context.rcx;
-    },
-	'post': function (return_value) {
-		if (return_value == 0) {
-			return;
-		}
-		console.log('[+] call post UnityEngine.Input::GetMouseButtonDown(UnityEngine.KeyCode)');
-		//console.log("\nBacktrace:\n" + Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join("\n"));
-        //console.log('args[1]  : ' + module_function_windows[1].global[this.context.ebp]);
-        console.log('return_value : ' + return_value);
-	} },
-{'global': {}, 'module': 'GameAssembly.dll', 'offset': 0x014BDD00,
-	'pre': function (args) {
-        //console.log('[+] call pre  UnityEngine.Input::GetMouseButtonUp(UnityEngine.KeyCode)');
-        module_function_windows[1].global[this.context.ebp] = this.context.rcx;
-    },
-	'post': function (return_value) {
-		if (return_value == 0) {
-			return;
-		}
-		console.log('[+] call post UnityEngine.Input::GetMouseButtonUp(UnityEngine.KeyCode)');
-        console.log('args[1]  : ' + module_function_windows[1].global[this.context.ebp]);
-        console.log('return_value : ' + return_value);
-	} },
-{'global': {}, 'module': 'GameAssembly.dll', 'offset': 0x01497BA0,
-	'pre': function (args) {
-        //console.log('[+] call pre  UnityEngine.Input::GetMouseButtonUp(UnityEngine.KeyCode)');
-        module_function_windows[1].global[this.context.ebp] = this.context.rcx;
-    },
-	'post': function (return_value) {
-		if (return_value == 0) {
-			return;
-		}
-		console.log('[+] call post LoadSceneAsyncNameIndexInternal_Injected 1');
-        console.log('args[1]  : ' + module_function_windows[1].global[this.context.ebp]);
-        console.log('return_value : ' + return_value);
-	} },
-{'global': {}, 'module': 'GameAssembly.dll', 'offset': 0x01497C10,
-	'pre': function (args) {
-        //console.log('[+] call pre  UnityEngine.Input::GetMouseButtonUp(UnityEngine.KeyCode)');
-        module_function_windows[1].global[this.context.ebp] = this.context.rcx;
-    },
-	'post': function (return_value) {
-		if (return_value == 0) {
-			return;
-		}
-		console.log('[+] call post LoadSceneAsyncNameIndexInternal_Injected 2');
-        console.log('args[1]  : ' + module_function_windows[1].global[this.context.ebp]);
-        console.log('return_value : ' + return_value);
-	} },
-{'global': {}, 'module': 'GameAssembly.dll', 'offset': 0x01497C70,
-	'pre': function (args) {
-        //console.log('[+] call pre  UnityEngine.Input::GetMouseButtonUp(UnityEngine.KeyCode)');
-        module_function_windows[1].global[this.context.ebp] = this.context.rcx;
-    },
-	'post': function (return_value) {
-		if (return_value == 0) {
-			return;
-		}
-		console.log('[+] call post LoadSceneAsyncNameIndexInternal_Injected 3');
-        console.log('args[1]  : ' + module_function_windows[1].global[this.context.ebp]);
-        console.log('return_value : ' + return_value);
-	} },
-*/
-
 //  HOOK 示例
-{'global': {}, 'module': '', 'address': 0, 'pre': null, 'post': null},
-{'global': {}, 'module': '', 'offset': 0, 'pre': null, 'post': null},
-{'global': {}, 'module': '', 'func': '', 'pre': null, 'post': null}
+{'key' : '' , 'global': {}, 'module': '', 'address': 0, 'pre': null, 'post': null},
+{'key' : '' , 'global': {}, 'module': '', 'offset': 0, 'pre': null, 'post': null},
+{'key' : '' , 'global': {}, 'module': '', 'func': '', 'pre': null, 'post': null}
 ];
 
 //  HOOK 列表
@@ -222,30 +97,37 @@ var module_function_windows = [
 //
 //  https://frida.re/docs/javascript-api/
 var module_function_android = [
-    { 'class': 'com.lenovo.nfsserver.prog.NFS3Prog',
-        'init': function (class_object) {
-            let NFS3Prog = class_object;
-            NFS3Prog["procedureGETATTR"].implementation = function () {
-                console.log(`NFS3Prog.procedureGETATTR is called`);
-                this["procedureGETATTR"]();
+    {
+        'class': 'com.lenovo.hec.vault.api.FileStationManager',
+        'init': function (class_object)
+         {
+            let FileStationManager = class_object;
+            FileStationManager["handleHandshakeMessage"].implementation = function (deviceId, payload) {
+                console.log(`handleHandshakeMessage : deviceId[${deviceId}] : payload[${payload}]`);
+                this["handleHandshakeMessage"](deviceId, payload);
             };
-            /*
-            NFS3Prog["writeFattr3"].implementation = function (fileID, useCache) {
-                console.log(`NFS3Prog.writeFattr3 is called: fileID=${fileID} , useCache=${useCache}`);
-                this["writeFattr3"](fileID, useCache);
-            };
-            */
-        } , 'object' : null },
-    { 'class': 'com.lenovo.nfsserver.utils.LogUtils',
+
+            //NFS3Prog["writeFattr3"].implementation = function (fileID, useCache) {
+            //    console.log(`NFS3Prog.writeFattr3 is called: fileID=${fileID} , useCache=${useCache}`);
+            //    this["writeFattr3"](fileID, useCache);
+            //};
+        } ,
+        'object' : null
+    },
+    /*
+    {
+        'class': 'com.zui.continuity.board.util.LogUtils',
         'init': function (class_object){
             let LogUtils = class_object;
-            console.log(`LogUtils is called : isDebug = ${LogUtils.isDebug.value}`);
-            LogUtils.isDebug.value = false;
-            console.log(`LogUtils is called : isDebug = ${LogUtils.isDebug.value}`);
-        }, 'object' : null },
+            console.log(`LogUtils is called : isDebug = ${LogUtils.LOG_INFO.value}`);
+            LogUtils.LOG_INFO.value = true;
+            console.log(`LogUtils is called : isDebug = ${LogUtils.LOG_INFO.value}`);
+        },
+        'object' : null
+    },
+    */
     { 'class': '', 'init': null, 'object' : null }
 ];
-
 
 var module_run_event = {
     'pre' : function (system_env) {
@@ -259,3 +141,6 @@ var module_run_event = {
     }
 };
 
+
+//  console.log(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()));                              //java打印堆栈
+//  console.log(' called from:\n' +Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\n') + '\n'); //SO打印堆栈
